19bbe73d85736eb4dc6cf83bde9cbdbcbdd631ea
GitHub Action <action@github.com>
2020-12-12 21:12:13 +0000

    Template cleanup

--- src/main/kotlin/com/github/tarasivashchuk/timer/services/MyProjectService.kt
+++ src/main/kotlin/com/github/tarasivashchuk/timer/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.tarasivashchuk.timer.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.tarasivashchuk.timer.MyBundle
 
 class MyProjectService(project: Project) {
 
